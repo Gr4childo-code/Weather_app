9e916fd285816840485db0eb58b46343ca63fccd
Samokhin Daniil <daniil.samokhin@decathlon.com>
2023-05-11 11:09:41 +0300

    fixes search

---
+++ src/components/Search/index.tsx
@@ -0,0 +1,31 @@
+import { useRef } from 'react';
+import React from 'react';
+import { RiSearchLine, RiMenu4Line } from 'react-icons/ri';
+
+import styles from '@/src/components/Search/Search.module.scss';
+
+const SearchForm = ({ search }) => {
+  const inputRef = useRef(null);
+
+  return (
+    <div className={styles.weather__header}>
+      <div className={styles.weather__header__content}>
+        <div className={styles.weather__header__content__search}>
+          <RiSearchLine
+            size={30}
+            className={styles.search__icon}
+            onClick={() => search(inputRef)}
+          />
+          <input
+            type='text'
+            className={styles.weather__header__content__search__input}
+            ref={inputRef}
+          />
+        </div>
+        <RiMenu4Line size={30} />
+      </div>
+    </div>
+  );
+};
+
+export default SearchForm;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,14 +1,27 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import Weather from '@/src/components/Weather';
 import getWeather from '@/utils/fetch';
+import SearchForm from '@/src/components/Search';
 
 export default function Home({ weather }) {
-  return <div className='container'>{weather && <Weather data={weather} />}</div>;
+  const [city, setCity] = useState(weather);
+
+  const handleSearchCity = async (e) => {
+    const weather = await getWeather(e.current.value);
+    setCity(weather);
+  };
+
+  return (
+    <div className='container'>
+      <SearchForm search={handleSearchCity} />
+      {city && <Weather data={city} search={handleSearchCity} />}
+    </div>
+  );
 }
 
 export const getServerSideProps = async () => {
-  const weather = await getWeather('kostanay');
+  const weather = await getWeather('Moscow');
   return {
-    props: { weather }
+    props: { weather },
   };
 };
